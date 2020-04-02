717b743e626de9e3299d570c178429a949a6554d
prosantomazumder <prosantomazumder@gmail.com>
2020-04-02 16:43:03 +0600

    version 1.0.1

---
+++ theme.scripts.js
@@ -0,0 +1,28 @@
+(function ($) {
+    "use strict";
+    
+    /******************************
+     *  Accordion Box  *
+    ******************************/
+    if($('.accordion-box').length){
+        $(".accordion-box").on('click', '.acc-btn', function() {
+            
+            var outerBox = $(this).parents('.accordion-box');
+            var target = $(this).parents('.accordion');
+            
+            if($(this).hasClass('active')!==true){
+                $(outerBox).find('.accordion .acc-btn').removeClass('active');
+            }
+            
+            if ($(this).next('.acc-content').is(':visible')){
+                return false;
+            }else{
+                $(this).addClass('active');
+                $(outerBox).children('.accordion').removeClass('active-block');
+                $(outerBox).find('.accordion').children('.acc-content').slideUp(300);
+                target.addClass('active-block');
+                $(this).next('.acc-content').slideDown(300);    
+            }
+        }); 
+    }
+})(jQuery);
---
+++ visual-composer-elements-helper.php
@@ -0,0 +1,58 @@
+<?php
+    function faqwithiconbox( $atts, $content=null ){
+
+        extract(shortcode_atts( array(
+        'faqinfo'		=> '',
+        'faqtitle'		=> '',
+        'faqcontent' 	=> '',
+        ), $atts ));
+
+        $contact_all_data 	= vc_param_group_parse_atts( $atts['faqinfo'] );
+
+       
+        ob_start(); ?>
+
+            <!--Accordian Box-->
+            <ul class="accordion-box">
+                <?php
+
+                    $Number = 1;
+
+                    foreach ( $contact_all_data as $contact_single_data ) :
+
+
+
+                        if( $Number == 1){
+                            $activeblock = 'active-block';
+                            $active = 'active';
+                            $current = 'current';
+
+                        }else{
+                            $activeblock = ' ';
+                            $active = ' ';
+                            $current = ' ';
+                        }
+
+                    ?>
+                        <!--Block-->
+
+                        <li class="accordion block <?php echo $activeblock; ?>">
+                            <div class="acc-btn <?php echo $active; ?>">
+                            	<div class="icon-outer">
+                            		<span class="icon fa-plus fa fa-plus"></span>
+                            		<span class="icon fa-minus fa fa-minus"></span>
+                            	</div><?php echo $contact_single_data['faqtitle']; ?>
+                        	</div>
+                            <div class="acc-content <?php echo $current; ?>">
+                                <div class="content">
+                                    <div class="accordian-text"><?php echo $contact_single_data['faqcontent']; ?></div>
+                                </div>
+                            </div>
+                        </li>
+                    <?php $Number++; endforeach;
+                ?>
+            </ul>
+        <?php return ob_get_clean();
+    }
+
+    add_shortcode( 'faqwithicon', 'faqwithiconbox' );
