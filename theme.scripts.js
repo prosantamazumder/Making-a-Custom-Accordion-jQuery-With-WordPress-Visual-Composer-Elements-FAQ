35eda4bbea002613280a3b03a6503712af067b73
prosantomazumder <prosantomazumder@gmail.com>
2020-04-05 00:59:24 +0600

    version 2.1.0

--- theme.scripts.js
+++ theme.scripts.js
@@ -1,6 +1,17 @@
 (function ($) {
     "use strict";
 
+    /**!-------------------------------------------
+        01. PRELOADER
+    --------------------------------------------**/
+    var $mainwindow = $(window);
+    $mainwindow.on('load', function() {
+
+        $("#loader").fadeOut(1000);
+    });
+
+
+
     /************************************************
      *  Accordion Box JS ADD THIS CODE MAIN.JS FIELS  *
     ***************************************************/
